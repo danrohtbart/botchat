b3781a9f4b7605a40e89c3c617c2127eb2606eac
danrohtbart <dan@rohtbart.com>
2024-01-06 18:58:34 -0500

    Refactored the domain/address checker. Added Betsy, Avi, and Josh to the allowed list.

--- amplify/backend/function/botchatpresignup/src/index.js
+++ amplify/backend/function/botchatpresignup/src/index.js
@@ -7,13 +7,16 @@ exports.handler = (event, context, callback) => {
     // Split the email address so we can compare domains
     var address = event.request.userAttributes.email.split("@")
     var domain = address[1];
+
+    const acceptableDomains = ['rohtbart.com', 'aetion.com'];
+    const acceptableAddresses = ['aglazer@fourcubits.com', 'jets613@gmail.com', 'betsymorserohtbart@gmail.com'];
     
-    if (domain == "rohtbart.com" || domain == "aetion.com") {
+    if (acceptableDomains.includes(domain) || acceptableAddresses.includes(address)) {
         // Success! Return to Amazon Cognito
         callback(null, event);
     } else {
         // Fail!
-        callback("Error: Domain not allowed", null);
+        callback("Sorry, we are not yet open to the internet. Ask Dan.", null);
     }
 };
 
