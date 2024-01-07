3947ba9bea270c542e2d230496f234f259257f8a
danrohtbart <dan@rohtbart.com>
2024-01-07 14:01:50 -0500

    Significant improvements to prompt engineering. I think they're working, but had to stop testing because it's time for Fortnite.

--- amplify/backend/function/botchattriggerjs/src/index.js
+++ amplify/backend/function/botchattriggerjs/src/index.js
@@ -14,10 +14,11 @@ const { BedrockRuntimeClient, InvokeModelCommand }  = require('@aws-sdk/client-b
 const { Amplify } = require('aws-amplify');
 const { generateClient } = require('aws-amplify/api');
 
+// Set these 4 to false for normal production operation
 const debug = true;
 const mock_bedrock = false;
 const drain_queue = false;
-const prevent_write = true;
+const prevent_write = false;
 
 if (debug) {
     console.log('Loading botchattriggerjs.');
@@ -105,6 +106,10 @@ const listChats = /* GraphQL */ `
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
+    /**
+     * Initialization section. Quickly return if the result can't be 200. 
+     */
+
     if (event.Records[0].eventName == "REMOVE") {
             console.log("This is a REMOVE event. Ignoring it.");
             return {
@@ -132,7 +137,16 @@ exports.handler = async (event) => {
         };
     }
 
-    // Retrieve personalities using GraphQL
+    // Default personalities, in case there is no personality for this user
+    let name_1 = "Jim";
+    let personality_1 = "You are a sports talk radio host from Philadelphia, named Jim Hoagies. You should respond like a jerk. You have strong opinions, and do not present counter-arguments.";
+    let name_2 = "Mark";
+    let personality_2 = "You are a sports talk radio host from Philadelphia, named Mark Waterice. You are polite, smart, and firm. You have strong opinions, and do not present counter-arguments.";
+
+    /**
+     *  Retrieve personalities section
+     */
+
     const amplify_config = {
         "aws_project_region": process.env.REGION,
         "aws_appsync_graphqlEndpoint": process.env.API_BOTCHAT_GRAPHQLAPIENDPOINTOUTPUT,
@@ -147,10 +161,6 @@ exports.handler = async (event) => {
 
     const amplifyClient = generateClient();
 
-    let name_1 = "Jim";
-    let personality_1 = "You are a sports talk radio host from Philadelphia, named Jim Hoagies. You should respond like a jerk. You have strong opinions, and do not present counter-arguments.";
-    let name_2 = "Mark";
-    let personality_2 = "You are a sports talk radio host from Philadelphia, named Mark Waterice. You are polite, smart, and firm. You have strong opinions, and do not present counter-arguments.";
     const incoming_user_email = incoming_content.user_email.S || '';
     if (debug) {
         console.log("Incoming user email is", incoming_user_email);
@@ -194,6 +204,9 @@ exports.handler = async (event) => {
         console.log("Personality 2 is", personality_2);
     }
     
+    /**
+     * Now that we have the Personality, let's get started on the Chat
+     */
 
     const last_statement = incoming_content.message.S || '';
     const last_speaker = incoming_content.speaker_name.S || '';
@@ -210,6 +223,7 @@ exports.handler = async (event) => {
     }
 
     if (message_in_thread > max_thread) {
+        // Future optimization: move this to Initialization
         console.log("Stopping the conversation after", message_in_thread, "statements.");
         return {
             statusCode: 204
@@ -236,52 +250,105 @@ exports.handler = async (event) => {
 <</SYS>>
 
 {{ user_msg_1 }} [/INST] {{ model_answer_1 }} </s><s>[INST] {{ user_msg_2 }} [/INST]
-
+        * ...which we think can be repeated for any number of model_answer_n and user_msg_n+1.
+        *
+        * Because the model is expecting to be the second speaker, we can only pass an odd number of messages. Of course, arrays are zero-indexed, so we get to be confused by odds and evens :facepalm:, so we need to be careful when talking about the array chat_messages.length vs the index that iterates through. 
+        * The simple case is when we have only chat_messages.length==1, because that is the beginning of the thread. 
+        * The normal case is when chat_messages.length is odd. 
+        * But we need a trade-off in this implementation, when chat_messages.length is even. In that case, we will skip the original user prompt (array index 0, message_in_thread 0). 
         */        
 
-        let prompt = "<s>[INST]";
-        let instruction = " <<SYS>> \n" + speaker_personality + "\nDo not mention specific people who were alive when the model was trained. Do not repeat the prompt. Your response should only be one person speaking. You should respond to this opinion: <</SYS>>\n\n";
+        // The basic prompt is `<s>[INST] <<SYS>>\n{{ system_prompt }}\n<</SYS>>\n\n" 
+        // This is where we insert the extra instructions that we give to all bots, based on all the fun experiments. 
+        let prompt = "<s>[INST] <<SYS>> \n" + speaker_personality + ". Do not mention specific people who were alive when the model was trained. Do not repeat the prompt. Your response should only be one person speaking. \n<</SYS>>\n\n";
 
         if(debug) {
             console.log("Speaker name is", speaker_name);
-            console.log("Instruction is", instruction);
+            console.log("Prompt starts as", prompt);
         }
-
-        prompt += instruction;
         
-        // Retrieve all chats in this thread_id, sorted in order of message_in_thread. Iterate through them by message_in_thread,  appending them to prompt. Remove any line breaks, just in case.
-        try {
-            const all_chats = await amplifyClient.graphql({
-                query: listChats,
-                variables: {
-                    filter: {
-                        thread_id: { eq: thread_id }
-                        // user_email: { eq: incoming_user_email } // this is the authenticated user's email address
+        if (message_in_thread == 0) {
+            // Simple case. User has just asked the question. 
+            prompt += last_statement.replace(/\n/g, ' ') + " [/INST] ";
+        } else {
+            // Retrieve all chats in this thread_id, sorted in order of message_in_thread. Iterate through them by message_in_thread,  appending them to prompt. Remove any line breaks, just in case.
+            try {
+                const all_chats = await amplifyClient.graphql({
+                    query: listChats,
+                    variables: {
+                        filter: {
+                            thread_id: { eq: thread_id }
+                            // user_email: { eq: incoming_user_email } // this is the authenticated user's email address
+                        },
                     },
-                },
-            });
-            if (debug) {
-                console.log ("all_chats ", all_chats.data.listChats.items);
-            }
-            let chat_messages = all_chats.data.listChats.items;
-            chat_messages.sort(function(a, b) {
-                return a.message_in_thread - b.message_in_thread;
-            });
-            if (debug) {
-                console.log ("Sorted chat_messages ", chat_messages);
-            }
-/*            chat_messages.forEach(function(chat_message) {
-                prompt += chat_message.message.replace(/\n/g, ' ') + "\n";
-            }
-            if (debug) {
-                console.log("Prompt is", prompt);
-            }*/
-        } catch (error) {
-            console.log("Error retrieving chats", error);
-        }
-        
-        prompt += last_statement.replace(/\n/g, ' ') + "[/INST]";
+                });
+                if (debug) {
+                    console.log ("all_chats ", all_chats.data.listChats.items);
+                }
+                let chat_messages = all_chats.data.listChats.items;
+                chat_messages.sort(function(a, b) {
+                    return a.message_in_thread - b.message_in_thread;
+                });
+                if (debug) {
+                    console.log ("Sorted chat_messages ", chat_messages);
+                }
 
+                if (chat_messages.length % 2 == 1) {
+                    // This is the normal case, when there are an odd number of messages in thread.  
+                    if (debug) {
+                        console.log("chat_messages.length is odd", chat_messages.length);
+                    }
+                    
+                    // Handle the slightly unusual first message
+                    prompt += chat_messages[0].message.replace(/\n/g, ' ') + " [/INST] ";
+
+                    // Iterate through the rest of the messages IN PAIRS, appending them to the prompt.
+                    for (let i = 1; i < chat_messages.length - 1; i += 2) {
+                        if (debug) {
+                            console.log("i is", i);
+                            console.log("chat_messages[i].message is", chat_messages[i].message);
+                            console.log("chat_messages[i+1].message is", chat_messages[i+1].message);
+                        }
+                        prompt += chat_messages[i].message.replace(/\n/g, ' ') + " </s><s>[INST] " + chat_messages[i + 1].message.replace(/\n/g, ' ') + " [/INST] ";
+                    }
+                } else {
+                    // Trade-off case: chat_messages is even and >0. To prompt the bot correctly, we're skipping the zero'th element in chat_messages. Same loop as the prior case, except the elements chose from the array are different.
+                    if (debug) {
+                        console.log("chat_messages.length is even", chat_messages.length);
+                    } 
+
+                    // Handle the slightly unusual first message
+                    prompt += chat_messages[1].message.replace(/\n/g, ' ') + " [/INST] ";
+                    
+                    // Iterate through the rest of the messages IN PAIRS, appending them to the prompt.
+                    for (let i = 2; i < chat_messages.length - 1; i += 2) {
+                        if (debug) {
+                            console.log("i is", i);
+                            console.log("chat_messages[i].message is", chat_messages[i].message);
+                            console.log("chat_messages[i+1].message is", chat_messages[i+1].message);
+                        }
+                        prompt += chat_messages[i].message.replace(/\n/g, ' ') + " <s>[INST] " + chat_messages[i + 1].message.replace(/\n/g, ' ') + " [/INST] ";
+                    }
+                }
+
+                if (debug) {
+                    console.log("Prompt is", prompt);
+                    console.log("End of the prompt.");
+                }
+            } catch (error) {
+                prompt += last_statement.replace(/\n/g, ' ') + " [/INST] ";
+                console.log("Warning: unable to retrieve chats. This bot will respond based on the last message, without any additional context. ", error);
+            }
+        }
+
+        if (debug) {
+            console.log("Prompt is", prompt);
+        }
+
+        /**
+         * Configure the bot in Bedrock
+         */
+            
         bedrock_request_body = {
             body: JSON.stringify({
                 prompt: prompt,
