b35a1e1b0ca24f93685097437bf7e9d19d2b7b38
danrohtbart <dan@rohtbart.com>
2024-05-11 11:06:45 -0400

    Oops, that last one went too soon

--- amplify/backend/function/botchattriggerjs/src/index.js
+++ amplify/backend/function/botchattriggerjs/src/index.js
@@ -388,8 +388,8 @@ exports.handler = async (event) => {
         const aws_sdk_config = {
             region: 'us-east-1',
             credentials: {
-                accessKeyId: Parameters.find(param => param.Name.includes('AccessKeyId')).Value,
-                secretAccessKey: Parameters.find(param => param.Name.includes('secretAccessKey')).Value
+                accessKeyId: Parameters.find(param => param.Name.includes('AMPLIFY_botchattriggerjs_AccessKeyId')).Value,
+                secretAccessKey: Parameters.find(param => param.Name.includes('AMPLIFY_botchattriggerjs_secretAccessKey')).Value
             }
         }
 
