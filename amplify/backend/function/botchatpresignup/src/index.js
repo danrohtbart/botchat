ec398aa2647bf627f9641593bef6dd622e0dcb95
danrohtbart <dan@rohtbart.com>
2024-07-06 10:32:41 -0400

    Made the AcceptableDomains and AcceptableAddresses for new accounts case-insensitive.

--- amplify/backend/function/botchatpresignup/src/index.js
+++ amplify/backend/function/botchatpresignup/src/index.js
@@ -5,12 +5,13 @@
  */
 exports.handler = (event, context, callback) => {
     // Split the email address so we can compare domains
-    var email_address = event.request.userAttributes.email;
+    var email_address = event.request.userAttributes.email.toLowerCase();
     var address = event.request.userAttributes.email.split("@")
-    var domain = address[1];
+    var domain = address[1].toLowerCase();
 
+    // AcceptableDomains and AcceptableAddresses must be in lowercase
     const acceptableDomains = ['rohtbart.com', 'aetion.com'];
-    const acceptableAddresses = ['Bobschwartz314@gmail.com', 'aglazer@fourcubits.com', 'jets613@gmail.com', 'betsymorserohtbart@gmail.com'];
+    const acceptableAddresses = ['bobschwartz314@gmail.com', 'aglazer@fourcubits.com', 'jets613@gmail.com', 'betsymorserohtbart@gmail.com'];
     
     if (acceptableDomains.includes(domain) || acceptableAddresses.includes(email_address)) {
         // Success! Return to Amazon Cognito
