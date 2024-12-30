138884e2dd2a3cc64f6f24817e40341872aaafc9
danrohtbart <dan@rohtbart.com>
2024-12-30 15:21:59 -0500

    Added a login test for playwright

---
+++ e2e/login.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+// TODO: externalize this test account u/p into environment variables or secrets. But TBH the blast radius is nonexistent. 
+
+test('login', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('tab', { name: 'Sign In' }).click();
+  await page.getByPlaceholder('Enter your Email').click();
+  await page.getByPlaceholder('Enter your Email').fill('TEST_USER_EMAIL');
+  await page.getByPlaceholder('Enter your Password').click();
+  await page.getByPlaceholder('Enter your Password').click();
+  await page.getByPlaceholder('Enter your Password').fill('TEST_USER_PASSWORD');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.getByText('Talk: Sign out Delete Chats')).toBeVisible();
+  await expect(page.getByText('Bot Personality ControlsName')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Sign out' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Delete Chats' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Update Personalities' })).toBeVisible();
+});
