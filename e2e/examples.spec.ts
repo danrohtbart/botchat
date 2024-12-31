e6230227acf9c8f042d094a55282eddc4625271f
danrohtbart <dan@rohtbart.com>
2024-12-30 19:54:48 -0500

    Submitting with only tests that pass

--- e2e/examples.spec.ts
+++ e2e/examples.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-/*  
+
 // This test should always pass
 test('always-pass', async ({ page }) => {
   await expect(true).toBe(true);
 });
-
+/*
 // This test should always fail
 test('always-fail', async ({ page }) => {
   await expect(true).toBe(false);
